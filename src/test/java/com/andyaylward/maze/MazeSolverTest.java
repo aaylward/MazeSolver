6566f44788fecb192880f2a85f050dd85a5484f3
Andy Aylward <aaylward@gmail.com>
2016-02-28 21:52:56 -0500

    add MazreReader::readMazeFromFile :hamster:

--- src/test/java/com/andyaylward/maze/MazeSolverTest.java
+++ src/test/java/com/andyaylward/maze/MazeSolverTest.java
@@ -1,61 +1,50 @@
 package com.andyaylward.maze;
 
-import com.andyaylward.maze.core.FillablePoint;
 import com.andyaylward.maze.core.Maze;
 import com.andyaylward.maze.core.Point;
 import com.andyaylward.maze.core.SolveStatistics;
+import com.andyaylward.maze.io.MazeReader;
+import com.google.common.io.Resources;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import java.time.Clock;
+import java.util.Optional;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MazeSolverTest {
-  private static final int SIZE = 5;
-  private FillablePoint[][] rows = new FillablePoint[SIZE][SIZE];
+  @Mock
+  private Scanner fakeScanner;
   private MazeSolver mazeSolver;
+  private MazeReader mazeReader;
 
   @Before
   public void setUp() {
+    mazeReader = new MazeReader(fakeScanner);
     mazeSolver = new MazeSolver(Clock.systemUTC());
-    //    . . . x x
-    //    . x . . .
-    //    . x x x .
-    //    . x s . .
-    //    e x x x .
-    for (int i=0; i<SIZE; i++) {
-      for (int j=0; j<SIZE; j++) {
-        rows[i][j] = new FillablePoint(i, j, false);
-      }
-    }
-
-    emptyPoint(0, 0);
-    emptyPoint(0, 1);
-    emptyPoint(0, 2);
-    emptyPoint(1, 0);
-    emptyPoint(1, 2);
-    emptyPoint(1, 3);
-    emptyPoint(1, 4);
-    emptyPoint(2, 0);
-    emptyPoint(2, 4);
-    emptyPoint(3, 0);
-    emptyPoint(3, 2);
-    emptyPoint(3, 3);
-    emptyPoint(3, 4);
-    emptyPoint(4, 0);
-    emptyPoint(4, 4);
   }
 
   @Test
-  public void itCanFindAnOnlyPath() {
+  public void itCanFindAnOnlyPath() throws URISyntaxException {
+    Maze singlePath = mazeReader.readMazeFromFile(Paths.get(Resources.getResource("singlepath.maze").toURI()));
     Point start = new Point(3, 2);
     Point end = new Point(4, 0);
-    SolveStatistics solveStatistics = mazeSolver.shortestPath(new Maze(rows), start, end);
-    assertThat(solveStatistics.getShortestPathLength()).isEqualTo(13);
+    Optional<SolveStatistics> solveStatisticsMaybe = mazeSolver.shortestPath(singlePath, start, end);
+    assertThat(solveStatisticsMaybe).isPresent();
+    assertThat(solveStatisticsMaybe.get().getShortestPathLength()).isEqualTo(13);
   }
 
-  private void emptyPoint(int x, int y) {
-    rows[x][y] = new FillablePoint(x, y, true);
+  @Test
+  public void itCannotFindPathsIfThereAreNon() throws URISyntaxException {
+    Maze singlePath = mazeReader.readMazeFromFile(Paths.get(Resources.getResource("nopath.maze").toURI()));
+    Point start = new Point(2, 2);
+    Point end = new Point(4, 0);
+    Optional<SolveStatistics> solveStatisticsMaybe = mazeSolver.shortestPath(singlePath, start, end);
+    assertThat(solveStatisticsMaybe).isEmpty();
   }
 }
