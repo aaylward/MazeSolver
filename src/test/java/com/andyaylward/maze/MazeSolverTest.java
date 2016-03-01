b873cedb6d0d98ab97c8d0269fbc534c26d9e509
Andy Aylward <aaylward@gmail.com>
2016-02-29 19:04:16 -0500

    that was very painful

--- src/test/java/com/andyaylward/maze/MazeSolverTest.java
+++ src/test/java/com/andyaylward/maze/MazeSolverTest.java
@@ -3,12 +3,14 @@ package com.andyaylward.maze;
 import com.andyaylward.maze.core.Maze;
 import com.andyaylward.maze.core.Point;
 import com.andyaylward.maze.core.SolveStatistics;
+import com.andyaylward.maze.io.MazeConsole;
 import com.andyaylward.maze.io.MazeReader;
 import com.google.common.io.Resources;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.time.Clock;
@@ -20,13 +22,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MazeSolverTest {
   @Mock
   private Scanner fakeScanner;
+  @Mock
+  private PrintStream fakePrintStream;
   private MazeSolver mazeSolver;
   private MazeReader mazeReader;
+  private MazeConsole mazeConsole;
 
   @Before
   public void setUp() {
     mazeReader = new MazeReader(fakeScanner);
     mazeSolver = new MazeSolver(Clock.systemUTC());
+    mazeConsole = new MazeConsole(mazeReader, fakePrintStream);
   }
 
   @Test
@@ -37,6 +43,8 @@ public class MazeSolverTest {
     Optional<SolveStatistics> solveStatisticsMaybe = mazeSolver.shortestPath(singlePath, start, end);
     assertThat(solveStatisticsMaybe).isPresent();
     assertThat(solveStatisticsMaybe.get().getShortestPathLength()).isEqualTo(13);
+
+    mazeConsole.writeSolutionToMaze(solveStatisticsMaybe.get());
   }
 
   @Test
